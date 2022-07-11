8acd93a46f975fca645ba331d31d48d460293a54
Vitaliy Kulinich <vitaliy.kulinich88@gmail.com>
2022-07-11 20:46:31 +0300

    Added start files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './css/styles.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from 'notiflix';
+import createGallary from './js/createGallary';
+import gallaryImages from './js/gallaryImages';
